97f0a4cd2b3c5f72048dee9066044a35f7441e8d
Milosz Dura <miloszdura@gmail.com>
2017-10-05 20:29:31 +0100

    adding tests around the communication response object.

---
+++ ShoppingCart.Core.Tests/Communication/GivenACommunicationResponse/WhenTheAddErrorMethodIsCalledWithAValidErrorMessage.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using ShoppingCart.Core.Communication;
+
+namespace ShoppingCart.Core.Tests.Communication.GivenACommunicationResponse
+{
+    [TestFixture]
+    public class WhenTheAddErrorMethodIsCalledWithAValidErrorMessage
+    {
+        private CommunicationResponse _subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _subject = new CommunicationResponse();
+
+            _subject.AddError(new Error
+            {
+                Message = "Some error message has occured."
+            });
+        }
+
+        [Test]
+        public void ThenTheHasErrorPropertyIsSetToTrue()
+        {
+            Assert.That(_subject.HasError, Is.True);
+        }
+
+        [Test]
+        public void ThenTheErrorMessageIsMappedCorreclty()
+        {
+            Assert.That(_subject.Error.Message, Is.EqualTo("Some error message has occured."));
+        }
+    }
+}
