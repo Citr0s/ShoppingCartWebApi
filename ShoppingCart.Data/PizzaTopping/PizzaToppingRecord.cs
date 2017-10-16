b22ef2e5e5059a847f44bff526b0ff347d9d7dec
Milosz Dura <miloszdura@gmail.com>
2017-10-16 15:28:56 +0100

    adding pizza topping link table mapper

---
+++ ShoppingCart.Data/PizzaTopping/PizzaToppingRecord.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using ShoppingCart.Data.Pizza;
+using ShoppingCart.Data.Topping;
+
+namespace ShoppingCart.Data.PizzaTopping
+{
+    public class PizzaToppingRecord
+    {
+        public virtual int Id { get; set; }
+        public virtual PizzaRecord Pizza { get; set; }
+        public virtual ToppingRecord Topping { get; set; }
+    }
+
+    public class PizzaToppingRecordMap : ClassMap<PizzaToppingRecord>
+    {
+        public PizzaToppingRecordMap()
+        {
+            Table("pizza_topping");
+            Id(x => x.Id);
+            References(x => x.Pizza).Column("pizza_id").Not.LazyLoad();
+            References(x => x.Topping).Column("topping_id").Not.LazyLoad();
+        }
+    }
+}
