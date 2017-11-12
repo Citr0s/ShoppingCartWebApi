2c9d9fc4c20a86756aba62fd911da677436b18e2
Milosz Dura <miloszdura@gmail.com>
2017-11-12 08:46:00 +0000

    setting up order save repository

---
+++ ShoppingCart.Data/Order/BasketToppingRecord.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentNHibernate.Mapping;
+using ShoppingCart.Data.Topping;
+
+namespace ShoppingCart.Data.Order
+{
+    public class BasketToppingRecord
+    {
+        public virtual int Id { get; set; }
+        public virtual OrderRecord Order { get; set; }
+        public virtual ToppingRecord Topping { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class BasketToppingRecordMap : ClassMap<BasketToppingRecord>
+    {
+        public BasketToppingRecordMap()
+        {
+            Table("basket_toppings");
+            Id(x => x.Id);
+            References(x => x.Order).Column("order_id").Not.LazyLoad();
+            References(x => x.Topping).Column("topping_id").Not.LazyLoad();
+        }
+    }
+}
--- ShoppingCart.Data/Order/OrderRepository.cs
+++ ShoppingCart.Data/Order/OrderRepository.cs
@@ -1,5 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using ShoppingCart.Core.Communication;
 using ShoppingCart.Data.Database;
+using ShoppingCart.Data.Pizza;
+using ShoppingCart.Data.Size;
+using ShoppingCart.Data.Topping;
 using ShoppingCart.Data.User;
 
 namespace ShoppingCart.Data.Order
@@ -17,22 +22,51 @@ namespace ShoppingCart.Data.Order
 
         public SaveOrderResponse SaveOrder(SaveOrderRequest request)
         {
-            var basketRecord = new BasketRecord
+            var response = new SaveOrderResponse();
+
+            try
             {
-                DeliveryType = request.DeliveryType,
-                Total = request.GrandTotal,
-                Voucher = request.Voucher,
-                User = _database.Query<UserRecord>().First(x => x.Id == request.UserId)
-            };
-            var basketId = _database.Save(basketRecord).Id;
+                var basketRecord = new BasketRecord
+                {
+                    DeliveryType = request.DeliveryType,
+                    Total = request.GrandTotal,
+                    Voucher = request.Voucher,
+                    User = _database.Query<UserRecord>().First(x => x.Id == request.UserId)
+                };
+                var basketId = _database.Save(basketRecord).Id;
 
-            _database.Save(new OrderRecord());
+                foreach (var order in request.Orders)
+                {
+                    var orderRecord = new OrderRecord
+                    {
+                        Basket = _database.Query<BasketRecord>().First(x => x.Id == basketId),
+                        Pizza = _database.Query<PizzaRecord>().First(x => x.Id == order.PizzaId),
+                        Size = _database.Query<SizeRecord>().First(x => x.Id == order.SizeId),
+                        Total = order.SubTotal
+                    };
 
-            // save baskets
+                    var orderId = _database.Save(orderRecord).Id;
 
-            // save basketToppings
+                    foreach (var toppingId in order.ExtraToppingIds)
+                    {
+                        var basketToppingRecord = new BasketToppingRecord
+                        {
+                            Order = _database.Query<OrderRecord>().First(x => x.Id == orderId),
+                            Topping = _database.Query<ToppingRecord>().First(x => x.Id == toppingId)
+                        };
+                        _database.Save(basketToppingRecord);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                response.AddError(new Error
+                {
+                    Message = "Something went wrong when saving Order to database."
+                });
+            }
 
-            return new SaveOrderResponse();
+            return response;
         }
     }
 }
