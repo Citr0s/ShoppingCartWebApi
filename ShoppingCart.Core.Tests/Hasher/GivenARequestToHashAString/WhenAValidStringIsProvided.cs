d9f2517d8b0c70f5b9e43efca4d389aa76e433d6
Milosz Dura <miloszdura@gmail.com>
2017-11-13 13:34:02 +0000

    added tests to the hasher

---
+++ ShoppingCart.Core.Tests/Hasher/GivenARequestToHashAString/WhenAValidStringIsProvided.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace ShoppingCart.Core.Tests.Hasher.GivenARequestToHashAString
+{
+    [TestFixture]
+    public class WhenAValidStringIsProvided
+    {
+        private string _result;
+        private Core.Hasher.Hasher _subject;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _subject = new Core.Hasher.Hasher();
+            _result = _subject.Hash("SOME_STRING");
+        }
+
+        [Test]
+        public void ThenTheStringIsHashedCorrectly()
+        {
+            Assert.That(_result, Is.Not.EqualTo("SOME_STRING"));
+        }
+
+        [Test]
+        public void ThenTheSameResultCanBeAchievedEveryTime()
+        {
+            Assert.That(_result, Is.EqualTo(_subject.Hash("SOME_STRING")));
+        }
+    }
+}
